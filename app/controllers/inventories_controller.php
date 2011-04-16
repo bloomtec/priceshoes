56f7e8ceedcf9b94cae297b1a2ec3316f9fe4560
Julio Cesar Dominguez Giraldo <juliodominguez@bloomtec.com>
2011-04-16 13:30:51 -0500

    corregida la linea 8 de inventories_controller

--- app/controllers/inventories_controller.php
+++ app/controllers/inventories_controller.php
@@ -5,7 +5,7 @@ class InventoriesController extends AppController {
 	var $uses=array("Inventory","Gallery");
 	
 	function getInventoryID ( $product_id = null, $color_id = null, $talla_id = null ) {
-		return $this -> find('first', array('conditions' => array('product_id' => $product_id, 'talla_id' => $talla_id, 'color_id' => $color_id)));
+		return $this -> Inventory -> find('first', array('conditions' => array('product_id' => $product_id, 'talla_id' => $talla_id, 'color_id' => $color_id)));
 	}
 	
 	function index() {
